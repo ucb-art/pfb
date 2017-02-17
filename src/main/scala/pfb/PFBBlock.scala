0506031d82ee134d9e5cce3312ff627ef672fa25
Paul Rigge <rigge@berkeley.edu>
2017-02-16 22:06:57 -0800

    Update for refactoring

--- src/main/scala/pfb/PFBBlock.scala
+++ src/main/scala/pfb/PFBBlock.scala
@@ -10,16 +10,13 @@ import dspblocks._
 import dspjunctions._
 import dspblocks._
 
-class LazyPFBBlock[T <: Data : Ring : ConvertableTo]()(implicit p: Parameters) extends LazyDspBlock()(p) {
-  def controls = Seq()
-  def statuses = Seq()
-
-  lazy val module = new PFBBlock[T](this)
+class PFBBlock[T <: Data : Ring : ConvertableTo]()(implicit p: Parameters) extends DspBlock()(p) {
+  lazy val module = new PFBBlockModule[T](this)
 
 }
 
-class PFBBlock[T <: Data : Ring : ConvertableTo](outer: LazyDspBlock)(implicit p: Parameters)
-  extends GenDspBlock[T, T](outer)(p) with HasPFBParameters[T] {
+class PFBBlockModule[T <: Data : Ring : ConvertableTo](outer: DspBlock)(implicit p: Parameters)
+  extends GenDspBlockModule[T, T](outer)(p) with HasPFBParameters[T] {
 
   val module = Module(new PFB(genIn(), Some(genOut()), genTap, pfbConfig))
   
